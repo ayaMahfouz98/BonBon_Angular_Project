f8158cf73551317f043375c175bbebbfd5f7e3a4
OmniaLashen15 <omnia.lashen15@gmail.com>
2022-05-16 23:36:24 +0200

    UserOrder completed

--- src/app/Components/UserOrder/UserOrder.component.ts
+++ src/app/Components/UserOrder/UserOrder.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { OrderService } from 'src/app/Services/Order.service';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/Services/User.service';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-UserOrder',
@@ -10,14 +11,14 @@ import { UserService } from 'src/app/Services/User.service';
   styleUrls: ['./UserOrder.component.css']
 })
 export class UserOrderComponent implements OnInit {
-  constructor(private userService: UserService, private orderService :OrderService ,private router:Router) {
+  constructor(private userService: UserService, private orderService :OrderService ,private router:Router,private modalService:NgbModal) {
     this.ID=this.userService.UserId;
 
    }
   userOrders:any;
   selectedOrderID:any; 
    ID:any;
-
+   closeResult='';
 
   ngOnInit() {
     //==>HERE
@@ -30,12 +31,33 @@ export class UserOrderComponent implements OnInit {
      (err)=>{console.log(err)}
    );
   }
+
+  open(content: any) {
+    //console.log(content)
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
   openOrderDetails(prdId:number){
     this.router.navigate(['/OrderDetails/',prdId])
   }
 
+
 CancelOrder(id:any)
-  {
+{
   this.orderService.DeleteOrder(id).subscribe();
   window.location.reload();    
 }
